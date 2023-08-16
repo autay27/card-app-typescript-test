0074435df86fdba31364a5ec8f47186260ae1e51
autay27 <au.tay.27@gmail.com>
2023-08-16 19:38:29 +0100

    automate setting up db before testing, add one working test

--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -1,8 +1,34 @@
 import { server } from "../src/server"
+import { Entry } from "@prisma/client";
 import Prisma from "../src/db";
 
+beforeEach(async () => {
+
+  const sampleEntry : Entry = {
+    id : "sampleid1",
+    title : "foo",
+    description : "bar",
+    scheduled : new Date("2023-08-17T17:36:43.069Z"),
+    created_at : new Date("2023-08-16T17:36:43.069Z")
+  }
+
+  await Prisma.entry.deleteMany({});
+  await Prisma.entry.create({data: sampleEntry});
+
+
+})
+
 describe("server test", () => {
-  it("should assert 1 + 1 is 2", () => {
-    expect(1 + 1).toEqual(2);
+  it("can get rows", async () => {
+
+    const response = await server.inject({
+      method: 'GET',
+      url: '/get/'
+    })
+
+    const parsedResponse = await response.json()
+
+    expect(response.statusCode).toEqual(200)
+    expect(parsedResponse.length).toEqual(1)
   });
 });
