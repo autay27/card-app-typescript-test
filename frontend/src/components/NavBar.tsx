5a4c0d40f9699f7d4e7236bd9db84d138ce19652
autay27 <au.tay.27@gmail.com>
2023-08-16 18:39:57 +0100

    automatic formatting

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,26 +1,35 @@
-import {NavLink} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
-export default function NavBar(){
-  
-    return(
-      <nav className="flex justify-center gap-5">
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/'}>All Entries</NavLink>
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/create'}>New Entry</NavLink>
-        <button className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" onClick={toggle}>Dark/Light Mode</button>
-      </nav>
-    )
+export default function NavBar() {
+  return (
+    <nav className="flex justify-center gap-5">
+      <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={"/"}>
+        All Entries
+      </NavLink>
+      <NavLink
+        className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white"
+        to={"/create"}
+      >
+        New Entry
+      </NavLink>
+      <button
+        className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white"
+        onClick={toggle}
+      >
+        Dark/Light Mode
+      </button>
+    </nav>
+  );
 }
 
-
-function toggle(){
-
-  console.log(localStorage.darkTheme)
+function toggle() {
+  console.log(localStorage.darkTheme);
 
   if (localStorage.theme === "light") {
     localStorage.setItem("theme", "dark");
-    document.documentElement.classList.add('dark')
+    document.documentElement.classList.add("dark");
   } else {
     localStorage.setItem("theme", "light");
-    document.documentElement.classList.remove('dark')
+    document.documentElement.classList.remove("dark");
   }
 }
